bdba631a5b1a940bf8b952cf7f93ce01aa113baf
Michal Lulco <lulco@efabrica.sk>
2025-11-08 07:40:22 +0100

    Added Canteen

---
+++ modules/php/Cards/Canteen.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace BANG\Cards;
+
+use BANG\Models\GreenCard;
+
+class Canteen extends GreenCard
+{
+  public function __construct(?array $params = null)
+  {
+    parent::__construct($params);
+    $this->type = CARD_CANTEEN;
+    $this->name = clienttranslate('Canteen');
+    $this->text = clienttranslate('Regain one life point.');
+    $this->symbols = [[SYMBOL_LIFEPOINT]];
+    $this->copies = [
+      BASE_GAME => [],
+      HIGH_NOON => [],
+      DODGE_CITY => ['7H', '7H', '7H', '7H', '7H', '7H', '7H', '7H', '7H', '7H', '7H', '7H', '7H', '7H', '7H', '7H', '7H', '7H', '7H', '7H', '7H'],
+    ];
+    $this->effect = [
+      'type' => LIFE_POINT_MODIFIER,
+      'amount' => 1,
+      'impacts' => NONE,
+    ];
+  }
+}
