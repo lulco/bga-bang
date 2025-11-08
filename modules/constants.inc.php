a1cb602983f22891c692e94a43bca9add2765b34
Michal Lulco <lulco@efabrica.sk>
2025-11-08 16:21:53 +0100

    removed empty line

--- modules/constants.inc.php
+++ modules/constants.inc.php
@@ -156,7 +156,6 @@ define('CARD_PONY_EXPRESS', 32);
 define('CARD_SOMBRERO', 33);
 define('CARD_IRON_PLATE', 34);
 define('CARD_TEN_GALLON_HAT', 35);
-
 define('CARD_CANTEEN', 36);
 
 // Event cards
