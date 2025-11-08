7141a7dc4446f964702ddae6c54c457ff65fada5
Michal Lulco <lulco@efabrica.sk>
2025-11-08 10:37:22 +0100

    Added Conestoga and Can Can

--- modules/constants.inc.php
+++ modules/constants.inc.php
@@ -153,6 +153,9 @@ define('CARD_RAG_TIME', 29);
 define('CARD_BINOCULAR', 30);
 define('CARD_HIDEOUT', 31);
 
+define('CARD_CONESTOGA', 40);
+define('CARD_CAN_CAN', 41);
+
 // Event cards
 define('CARD_BLESSING', 100);
 define('CARD_HANGOVER', 101);
