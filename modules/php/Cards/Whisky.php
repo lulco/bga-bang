dbf2f47b7446ddebcee4eb7b57021b52647d2ea6
Michal Lulco <lulco@efabrica.sk>
2025-11-15 17:10:35 +0100

    Whisky

--- modules/php/Cards/Whisky.php
+++ modules/php/Cards/Whisky.php
@@ -1,20 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace BANG\Cards;
 
-use BANG\Core\Notifications;
 use BANG\Managers\Cards;
-use BANG\Managers\Players;
-use BANG\Managers\Rules;
 use BANG\Models\AbstractCard;
 use BANG\Models\BrownCard;
 use BANG\Models\Player;
 
 class Whisky extends BrownCard
 {
-  public function __construct($id = null)
+  public function __construct(?array $params = null)
   {
-    parent::__construct($id);
+    parent::__construct($params);
     $this->type = CARD_WHISKY;
     $this->name = clienttranslate('Whisky');
     $this->text = clienttranslate('Regain two life points.');
@@ -31,27 +30,22 @@ class Whisky extends BrownCard
     ];
   }
 
-  public function play($player, $args)
+  public function play(Player $player, array $args): void
   {
-    if (!isset($args['secondCardId'])) {
+    if (!isset($args['additionalCardId'])) {
       return;
     }
 
-    $card = Cards::get($args['secondCardId']);
-    $card->discard();
-    Notifications::discardedCard($player, $card);
+    $additionalCard = Cards::get($args['additionalCardId']);
+    $player->discardCard($additionalCard);
 
     parent::play($player, $args);
   }
 
-  /**
-   * @param Player $player
-   */
-  public function getPlayOptions($player)
+  public function getPlayOptions(Player $player): ?array
   {
     $options = parent::getPlayOptions($player);
-    $options['with_another_card'] = [
-      'strict' => true,
+    $options['with_additional_card'] = [
       'cards' => $player->getHand()->filter(function (AbstractCard $card) {
           return $card->getId() !== $this->getId();
       })->toArray(),
