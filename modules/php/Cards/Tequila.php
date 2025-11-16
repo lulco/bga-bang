5fa4382f8bbb6be766f45e335b143b8b7b594a2d
Michal Lulco <lulco@efabrica.sk>
2025-11-16 13:57:53 +0100

    Used generic play and play options for tequila

--- modules/php/Cards/Tequila.php
+++ modules/php/Cards/Tequila.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 namespace BANG\Cards;
 
-use BANG\Managers\Cards;
-use BANG\Managers\Players;
-use BANG\Models\AbstractCard;
 use BANG\Models\BrownCard;
-use BANG\Models\Player;
 
 class Tequila extends BrownCard
 {
@@ -28,32 +24,7 @@ class Tequila extends BrownCard
       'type' => LIFE_POINT_MODIFIER,
       'amount' => 1,
       'impacts' => ANY,
+      'additional_card' => 1,
     ];
-  }
-
-  public function play(Player $player, array $args): void
-  {
-    if (!isset($args['additionalCardId'])) {
-      return;
-    }
-
-    $additionalCard = Cards::get($args['additionalCardId']);
-    $player->discardCard($additionalCard);
-
-    parent::play($player, $args);
-  }
-
-  public function getPlayOptions(Player $player): ?array
-  {
-    $options = [
-      'target_types' => [TARGET_PLAYER],
-      'targets' => Players::getLivingPlayers()->getIds(),
-      'with_additional_card' => [
-        'cards' => $player->getHand()->filter(function (AbstractCard $card) {
-          return $card->getId() !== $this->getId();
-        })->toArray(),
-      ]
-    ];
-    return $options;
   }
 }
